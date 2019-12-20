e5d409404a853e450d6d5ce71e76474f4482f981
Paolo Di Bello <dibellopol@gmail.com>
2019-12-20 16:00:50 +0100

    add service api on mount app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data: {
+    info: {
+
+    },
+    config: {
+      headers: {
+        'accept-language': 'it',
+        'content-type': 'application/json',
+        'x-musement-currency': 'EUR',
+        'x-musement-version': '3.4.0'
+      }
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.musement.com/api/v3/venues/164/activities?limit=6&offset=0', { config: this.config })
+      .then(response => (this.info = response))
+  }
+
 }).$mount('#app')
